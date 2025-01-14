c215fae256f648354da137717f8f1e475a239bb0
karanrajput1812 <Karan.Rajput@wissen.com>
2025-01-14 17:10:19 +0530

    Pattern Matching Tutorial Code Completed

--- 14_01_2025/PatternMatching.java
+++ 14_01_2025/PatternMatching.java
@@ -30,6 +30,36 @@ public class PatternMatching
 		Matcher m3 = p3.matcher(mobile_no+"");	
 		System.out.println(m3.matches());
 
+		System.out.println("------------------");
+		
+		String s5 = "ae";
+		String s6 = "[a-d][b-f]";
+		String s7 = "[a-d&&[b-f]]";
+		Pattern p4 = Pattern.compile(s6);
+		Pattern p5 = Pattern.compile(s7);
+		Matcher m4 = p4.matcher(s5);	
+		Matcher m5 = p5.matcher(s5);	
+
+		System.out.println(m4.matches());
+		System.out.println(m5.matches());
+
+		System.out.println("------------------");
+
+				
+		String s8 = "c";
+		String s9 = "[a-d][b-f]";
+		String s10 = "[a-d&&[b-f]]";
+		Pattern p6 = Pattern.compile(s9);
+		Pattern p7 = Pattern.compile(s10);
+		Matcher m6 = p6.matcher(s8);	
+		Matcher m7 = p7.matcher(s8);	
+
+		System.out.println(m6.matches());
+		System.out.println(m7.matches());
+
+		System.out.println("------------------");
+
+
 		
 		
 
