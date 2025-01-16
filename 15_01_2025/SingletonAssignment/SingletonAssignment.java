8c6028c69e5e05432ec4e8d921004917cacc0b95
karanrajput1812 <Karan.Rajput@wissen.com>
2025-01-16 17:57:54 +0530

    Singleton Assignment And Design Pattern Other Types

--- 15_01_2025/SingletonAssignment/SingletonAssignment.java
+++ 15_01_2025/SingletonAssignment/SingletonAssignment.java
@@ -1,73 +1,71 @@
-sealed class A permits B
-{
+sealed class A permits B {
 	static private A a1 = null;
-	
-	protected A()
-	{
-		System.out.println("Object A created");
+	protected A() {
+		if (this instanceof B) {
+			System.out.println("B instance created...........");
+		}
+		else if(this instanceof A && a1==null) {
+			System.out.println("A instance created...........");
+			a1 = this;
+		}
+		else {
+			throw new InstanceAlreadyExists("One Instace of A is already created...........");
+		}
 	}
-	public static A getObject()
-	{
-		try
-		{
-			if(a1 == null)
+
+	public static A getObject() {
+		try {
+			if (a1 == null)
 				a1 = new A();
 			else
-				throw new UserException("One Instace of A is already created");
-		}
-		catch (UserException e)
-		{
+				throw new InstanceAlreadyExists("One Instace of A is already created");
+		} catch (InstanceAlreadyExists e) {
 			System.out.println("Reason: " + e.getMessage());
 		}
 		return a1;
 	}
 }
-final class B extends A
-{
+final class B extends A {
 	static private B b1 = null;
-	private B()
-	{
-		System.out.println("Object B created");
+
+	private B() {
 	}
-	public static B getObject()
-	{
-		try
-		{
-			if(b1 == null)
+
+	public static B getObject() {
+		try {
+			if (b1 == null)
 				b1 = new B();
 			else
-				throw new UserException("One Instace of B is already created");
-		}
-		catch (UserException e)
-		{
+				throw new InstanceAlreadyExists("One Instace of B is already created");
+		} catch (InstanceAlreadyExists e) {
 			System.out.println("Reason: " + e.getMessage());
 		}
 		return b1;
-			
 	}
-
 }
 
-public class SingletonAssignment
-{
-	public static void main(String args[])
-	{
+public class SingletonAssignment {
+	public static void main(String args[]) {
 		A a1 = A.getObject();
 		A a2 = A.getObject();
 		B b1 = B.getObject();
 		B b2 = B.getObject();
+		A a3 = new A();
+		System.out.println(a1);
+		System.out.println(a2);
+		System.out.println(a3);
+		System.out.println(b1);
+		System.out.println(b2);
 
 	}
 }
 
-class UserException extends RuntimeException
-{
-	public UserException()
-	{
-		super(); 	
+class InstanceAlreadyExists extends RuntimeException {
+	public InstanceAlreadyExists() {
+		super();
 	}
-	public UserException(String msg)
-	{
+
+	public InstanceAlreadyExists(String msg) {
 		super(msg);
 	}
 }
