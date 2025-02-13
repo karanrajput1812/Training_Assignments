48ee1681a1aeba0f67fa4cda8fec6af30d1056d2
karanrajput1812 <Karan.Rajput@wissen.com>
2025-02-13 15:58:10 +0530

    Add JUnit and Mockito dependencies, implement initial tests for Calculator, and configure filtered resources

---
+++ 13_02_2025/assignment/src/test/java/assignment/CalculatorMainTest.java
@@ -0,0 +1,14 @@
+package assignment;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculatorMainTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
